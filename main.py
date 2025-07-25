901b0e6bee88a39f088da5ec5b58bda7b8334cb0
b-yarrow <byarrow86@gmail.com>
2025-07-25 20:29:15 +0900

    draw game screen

--- main.py
+++ main.py
@@ -8,6 +8,24 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    #while not(pygame.get_init()):
+        #do nothing, wait for 
+    
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    #game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+                 
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
+
+
+    
 
 
 if __name__ == "__main__":
